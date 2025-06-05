1319770ce8232dca68c76fb36acd1569984df00f
Brwni(Vini) <mavasquezba@est.utn.ac.cr>
2025-06-05 16:58:59 -0600

    Clase Locker
    Se crea la clase Locker con atributos, metodos de acceso, constructor y toString

---
+++ src/Lockers/Locker.java
@@ -0,0 +1,70 @@
+package Lockers;
+
+
+
+public class Locker {
+    private int id;
+    
+    public static final int SMALL = 10, MEDIUM = 20, LARGE = 30;
+    private int size;
+    
+    public static final int FREE=1, BUSY=2, OUTOFSERVICE=3;
+    private int state;
+    
+    private int packageId;
+    
+    private int userId; 
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSize() {
+        if (size <= Locker.SMALL) {
+            return "Small";
+        } else if (size > Locker.SMALL && size <= Locker.MEDIUM) {
+            return "Medium";
+        } else if (size > Locker.MEDIUM && size <= Locker.LARGE) {
+            return "Large";
+        } else {
+        return "Package to large";
+        }
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public int getPackageId() {
+        return packageId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public void setPackageId(int packageId) {
+        this.packageId = packageId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public Locker(int id, int size, int packageId, int userId) {
+        this.id = id;
+        this.size = size;
+        this.state = Locker.FREE;
+        this.packageId = 0;
+        this.userId = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Locker:" + "\nId: " + id + "\nSize:" + size + "\nState:" + state + "\nPackageId=" + packageId + "\nUserId=" + userId;
+    }
+}
